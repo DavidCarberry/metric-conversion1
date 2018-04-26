942381d1b6eb8b3b5ce4d1ce2267e24391a5ffe1
DavidCarberry <37617050+DavidCarberry@users.noreply.github.com>
2018-04-26 16:13:09 +0100

    All measurements added

--- js/convert.js
+++ js/convert.js
@@ -1,10 +1,29 @@
-function fToM()(
-
   var measure = parseInt(document.getElementById("value1").value);
-
+// Feet to meters
   var meters = measure x 0.3048;
-  var message = measure + ' feet converts to ' + meters + ' meters.';
+  var message = measure + ' feet converts to ' + ' meters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+   
+   var measure = parseInt(document.getElementById("value2").value);
+// inches to centimeters
+  var inches = measure x 2.54;
+  var message = measure + ' inches converts to '+ ' centimeters.';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+  //yards to meters
+ var measure = parseInt(document.getElementById("value3").value);
+
+  var Yards = measure x 0.9144;
+  var message = measure + ' yards converts to ' +' meters';
+  console.flag(message);
+  document.getElementbyId("resultsentence").innerHTML = message;
+  //miles to kilometers
+   var measure = parseInt(document.getElementById("value4").value);
+
+  var miles = measure x 1.60934;
+  var message = measure + ' miles converts to ' + ' kilometers';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
 
-)
+  function fToM() 
