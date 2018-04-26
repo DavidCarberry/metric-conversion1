5893f76af3e28ebfc86cae1ec5670d4da2d3087e
DavidCarberry <37617050+DavidCarberry@users.noreply.github.com>
2018-04-26 16:29:24 +0100

    Functions added, correction

--- js/convert.js
+++ js/convert.js
@@ -1,9 +1,15 @@
-  var measure = parseInt(document.getElementById("value1").value);
+  function fToM(){
+
+ var measure = parseInt(document.getElementById("value1").value);
 // Feet to meters
   var meters = measure x 0.3048;
   var message = measure + ' feet converts to ' + ' meters.';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
+  ]
+   
+   
+   function iToC(){
    
    var measure = parseInt(document.getElementById("value2").value);
 // inches to centimeters
@@ -11,19 +17,30 @@
   var message = measure + ' inches converts to '+ ' centimeters.';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
+   }
+  
+  
+  function yToM(){
   //yards to meters
- var measure = parseInt(document.getElementById("value3").value);
+  var measure = parseInt(document.getElementById("value3").value);
 
   var Yards = measure x 0.9144;
   var message = measure + ' yards converts to ' +' meters';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
-  //miles to kilometers
-   var measure = parseInt(document.getElementById("value4").value);
+  }
+ 
+
+
+
+	function mTok(){
+ //miles to kilometers
+  
+  var measure = parseInt(document.getElementById("value4").value);
 
   var miles = measure x 1.60934;
   var message = measure + ' miles converts to ' + ' kilometers';
   console.flag(message);
   document.getElementbyId("resultsentence").innerHTML = message;
 
-  function fToM() 
+  }
